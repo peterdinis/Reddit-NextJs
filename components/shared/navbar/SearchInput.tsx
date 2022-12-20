11061c15d5c91f47d79af30ed31c130f4ac49408
Peterdinis <peterdinis0@pobox.sk>
2022-12-20 19:58:52 +0100

    feat: Working on navbar

--- components/shared/navbar/SearchInput.tsx
+++ components/shared/navbar/SearchInput.tsx
@@ -1,17 +1,32 @@
-import { Flex } from '@chakra-ui/react'
-import React from 'react'
+import {
+  Flex,
+  Input,
+  InputGroup,
+  InputLeftAddon,
+  InputRightAddon,
+  Stack,
+} from "@chakra-ui/react";
+import React from "react";
 
-interface SearchInputProps {
-
-}
-
+interface SearchInputProps {}
 
 const SearchInput: React.FC<SearchInputProps> = () => {
   return (
     <Flex>
-        
-    </Flex>
-  )
-}
+      <Stack spacing={4}>
+        <InputGroup>
+          <InputLeftAddon children="+234" />
+          <Input type="tel" placeholder="phone number" />
+        </InputGroup>
 
-export default SearchInput
+        <InputGroup size="sm">
+          <InputLeftAddon children="https://" />
+          <Input placeholder="mysite" />
+          <InputRightAddon children=".com" />
+        </InputGroup>
+      </Stack>
+    </Flex>
+  );
+};
+
+export default SearchInput;
