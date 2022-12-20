101e3d9295bb64791afd5350259aa6e5f5069b29
Peterdinis <peterdinis0@pobox.sk>
2022-12-20 19:50:56 +0100

    feat: Working on navbar

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -1,13 +1,22 @@
 import { Flex, Image } from "@chakra-ui/react"
 import React from "react"
+import SearchInput from "./SearchInput"
+import Directory from "./Directory"
+import RightSidebar from "./RightSidebar"
 
 const Navbar: React.FC = () => {
     return (
         <Flex bg="white" height="44px" padding="8px 12px">
             <Flex align="center">
                 <Image src="https://raw.githubusercontent.com/shadeemerhi/reddit-clone-yt/dcaa8c107fb638b0541aca5d7aba9a49e70a7f81/public/images/redditFace.svg" height="30px" />
-                <Image src="https://raw.githubusercontent.com/shadeemerhi/reddit-clone-yt/dcaa8c107fb638b0541aca5d7aba9a49e70a7f81/public/images/redditText.svg" height="46px" />
+                <Image src="https://raw.githubusercontent.com/shadeemerhi/reddit-clone-yt/dcaa8c107fb638b0541aca5d7aba9a49e70a7f81/public/images/redditText.svg" height="46px" display={{
+                    base: "none", // mobile base size
+                    md: "unset" // display none
+                }} />
             </Flex>
+            <Directory />
+            <SearchInput />
+            <RightSidebar />
         </Flex>
     )
 }
---
+++ components/shared/navbar/SearchInput.tsx
@@ -0,0 +1,17 @@
+import { Flex } from '@chakra-ui/react'
+import React from 'react'
+
+interface SearchInputProps {
+
+}
+
+
+const SearchInput: React.FC<SearchInputProps> = () => {
+  return (
+    <Flex>
+        
+    </Flex>
+  )
+}
+
+export default SearchInput
