e0694fb3c05896b49e23ef5a81a127bc0fce26c8
Peterdinis <peterdinis0@pobox.sk>
2022-12-20 17:38:36 +0100

    FOO

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -1,11 +1,11 @@
-import { Flex } from "@chakra-ui/react"
+import { Flex, Image } from "@chakra-ui/react"
 import React from "react"
 
 const Navbar: React.FC = () => {
     return (
         <Flex bg="white" height="44px" padding="8px 12px">
             <Flex>
-                LOGO
+                <Image src="/redditlogo.png" height="30px" />
             </Flex>
         </Flex>
     )
