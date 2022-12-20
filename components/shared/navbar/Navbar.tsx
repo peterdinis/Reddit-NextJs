26dd79c0a7f11cd2c5595fa37fdcf4409295496e
Peterdinis <peterdinis0@pobox.sk>
2022-12-20 19:47:48 +0100

    feat: Navbar wip

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -4,8 +4,9 @@ import React from "react"
 const Navbar: React.FC = () => {
     return (
         <Flex bg="white" height="44px" padding="8px 12px">
-            <Flex>
-                <Image src="/redditlogo.png" height="30px" />
+            <Flex align="center">
+                <Image src="https://raw.githubusercontent.com/shadeemerhi/reddit-clone-yt/dcaa8c107fb638b0541aca5d7aba9a49e70a7f81/public/images/redditFace.svg" height="30px" />
+                <Image src="https://raw.githubusercontent.com/shadeemerhi/reddit-clone-yt/dcaa8c107fb638b0541aca5d7aba9a49e70a7f81/public/images/redditText.svg" height="46px" />
             </Flex>
         </Flex>
     )
