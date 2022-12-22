a12038c21ef81dd8105c3554d29fbad8aaa84c85
Peterdinis <peterdinis0@pobox.sk>
2022-12-22 21:32:48 +0100

    feat: Start adding firebase wip

--- firebase/init.ts
+++ firebase/init.ts
@@ -1,1 +1,18 @@
- import firebase from "firebase/app";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
