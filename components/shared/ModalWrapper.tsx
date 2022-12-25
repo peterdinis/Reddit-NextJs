f49900a3a5e03e964a688f62a98895efdf388790
Peterdinis <peterdinis0@pobox.sk>
2022-12-25 21:36:12 +0100

    feat: Modal Wrapper

---
+++ components/shared/ModalWrapper.tsx
@@ -0,0 +1,28 @@
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+  } from "@chakra-ui/react";
+  import React from "react";
+  
+  type ModalWrapperProps = {
+    isOpen: boolean;
+    onClose: () => void;
+    children?: React.ReactNode;
+  };
+  
+  const ModalWrapper: React.FC<ModalWrapperProps> = ({
+    children,
+    isOpen,
+    onClose,
+  }) => {
+    return (
+      <>
+        <Modal isOpen={isOpen} onClose={onClose} size="lg">
+          <ModalOverlay />
+          <ModalContent width={{ base: "sm", md: "xl" }}>{children}</ModalContent>
+        </Modal>
+      </>
+    );
+  };
+  export default ModalWrapper;
