b16499c410700ad0f3ea61414114443633ef9dea
Peterdinis <peterdinis0@pobox.sk>
2022-12-25 17:02:28 +0100

    FOO

---
+++ components/communities/CommunityModal.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CommunityModal() {
+  return (
+    <div>CommunityModal</div>
+  )
+}
+
+export default CommunityModal
--- components/shared/navbar/Directory.tsx
+++ components/shared/navbar/Directory.tsx
@@ -10,11 +10,70 @@ import {
   Text,
   Image,
 } from "@chakra-ui/react";
+import Communities from "../../communities/Communities";
+import useDirectory from '../../../hooks/useDirectory';
 
-function Directory() {
+const Directory: React.FC = () => {
+  const [open, setOpen] = React.useState(false);
+  const handleClose = () => setOpen(false);
+
+  const { directoryState, toggleMenuOpen } = useDirectory();
+
   return (
-    <div>Directory</div>
-  )
-}
-
-export default Directory
+    <Menu isOpen={directoryState.isOpen}>
+      {({ isOpen }) => (
+        <>
+          <MenuButton
+            cursor="pointer"
+            padding="0px 6px"
+            borderRadius="4px"
+            _hover={{ outline: "1px solid", outlineColor: "gray.200" }}
+            mr={2}
+            ml={{ base: 0, md: 2 }}
+            onClick={toggleMenuOpen}
+          >
+            <Flex
+              alignItems="center"
+              justifyContent="space-between"
+              width={{ base: "auto", lg: "200px" }}
+            >
+              <Flex alignItems="center">
+                <>
+                  {directoryState.selectedMenuItem.imageURL ? (
+                    <Image
+                      borderRadius="full"
+                      boxSize="24px"
+                      src={directoryState.selectedMenuItem.imageURL}
+                      mr={2}
+                    />
+                  ) : (
+                    <Icon
+                      fontSize={24}
+                      mr={{ base: 1, md: 2 }}
+                      color={directoryState.selectedMenuItem.iconColor}
+                      as={directoryState.selectedMenuItem.icon}
+                    />
+                  )}
+                  <Box
+                    display={{ base: "none", lg: "flex" }}
+                    flexDirection="column"
+                    fontSize="10pt"
+                  >
+                    <Text fontWeight={600}>
+                      {directoryState.selectedMenuItem.displayText}
+                    </Text>
+                  </Box>
+                </>
+              </Flex>
+              <ChevronDownIcon color="gray.500" />
+            </Flex>
+          </MenuButton>
+          <MenuList maxHeight="300px" overflow="scroll" overflowX="hidden">
+            <Communities menuOpen={isOpen} />
+          </MenuList>
+        </>
+      )}
+    </Menu>
+  );
+};
+export default Directory;
