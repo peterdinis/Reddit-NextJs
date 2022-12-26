713c25487d71bc9e51a358d44dccf00844d3becf
Peterdinis <peterdinis0@pobox.sk>
2022-12-26 14:18:26 +0100

    feat: 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,12 @@
+import { Text } from "@chakra-ui/react";
+import { NextPage } from "next";
+
+const NotFoundPage: NextPage = () => {
+    return (
+        <>
+            <Text>Page not Found</Text>
+        </>
+    )
+}
+
+export default NotFoundPage
