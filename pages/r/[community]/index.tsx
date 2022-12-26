6633f9f86c17bf69d397fdfc89864d4c7b86ef33
Peterdinis <peterdinis0@pobox.sk>
2022-12-26 18:56:10 +0100

    feat: Adding communities pages

---
+++ pages/r/[community]/index.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+
+const CommunityPage: NextPage = () => {
+    return (
+        <>
+        
+        </>
+    )
+}
---
+++ pages/r/[community]/submit.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const SubmitPage: NextPage = () => {
+    return (
+        <>
+        PING
+        </>
+    )
+}
+
+export default SubmitPage;
