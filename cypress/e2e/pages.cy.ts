b8539b1ec6944a5d91c3ef977394f92b6cc13b04
Peterdinis <peterdinis0@pobox.sk>
2022-12-26 14:21:57 +0100

    test: Updating cypress specs

---
+++ cypress/e2e/pages.cy.ts
@@ -0,0 +1,11 @@
+describe('Visit our reddit page', () => {
+    it('Visit homepage for our reddit clone', () => {
+      cy.visit('http://localhost:3000')
+    })
+  })
+
+describe("Visit our 404 page", () => {
+    it("Go to page which does not exists", () => {
+        cy.visit("http://localhost:3000/notExists")
+    })
+})
--- cypress/e2e/spec.cy.ts
+++
@@ -1,5 +0,0 @@
-describe('Visit our reddit page', () => {
-  it('Visit homepage for our reddit clone', () => {
-    cy.visit('http://localhost:3000')
-  })
-})
