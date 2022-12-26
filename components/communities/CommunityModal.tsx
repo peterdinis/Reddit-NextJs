232fd66ff33afad3e5787b06a2f27e1c2ed9f64e
Peterdinis <peterdinis0@pobox.sk>
2022-12-26 12:41:16 +0100

    feat: Start to fixing communityModal wip

--- components/communities/CommunityModal.tsx
+++ components/communities/CommunityModal.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React from "react";
+import {
+  Box,
+  Button,
+  Checkbox,
+  Divider,
+  Flex,
+  Icon,
+  Input,
+  ModalBody,
+  ModalCloseButton,
+  ModalFooter,
+  ModalHeader,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import { doc, runTransaction, serverTimestamp } from "firebase/firestore";
+import { useRouter } from "next/router";
+import { BsFillEyeFill, BsFillPersonFill } from "react-icons/bs";
+import { HiLockClosed } from "react-icons/hi";
+import { useSetRecoilState } from "recoil";
+import { firestore } from "../../firebase/init";
+import ModalWrapper from "../shared/ModalWrapper";
+import { communityState } from "../../recoil/atoms/communitiesAtom";
 
-function CommunityModal() {
+const CommunityModal: React.FC = () => {
   return (
-    <div>CommunityModal</div>
+    <>
+
+    </>
   )
 }
 
