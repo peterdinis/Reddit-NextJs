82cb84e9b758fde06406ed10b96bb2054e13ecd6
Peterdinis <peterdinis0@pobox.sk>
2022-12-29 17:17:17 +0100

    feat: Updating app

--- components/posts/AboutWrapper.tsx
+++
@@ -1,9 +0,0 @@
-const AboutWrapper: React.FC = () => {
-    return (
-        <>
-            EEEEE
-        </>
-    )
-}
-
-export default AboutWrapper;
--- components/posts/TabItem.tsx
+++
@@ -1,9 +0,0 @@
-const TabItem: React.FC = () => {
-    return (
-        <>
-            PING
-        </>
-    )
-}
-
-export default TabItem;
