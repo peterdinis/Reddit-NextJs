252c03b1ec051ab219177bf80d68b09a2d4908e6
Peterdinis <peterdinis0@pobox.sk>
2022-12-29 17:34:28 +0100

    feat: Updating posts components wip

---
+++ components/post/AllPosts.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Stack } from "@chakra-ui/react";
+import {
+  collection,
+  doc,
+  getDocs,
+  onSnapshot,
+  orderBy,
+  query,
+  where,
+  writeBatch,
+} from "firebase/firestore";
+import { useRecoilState, useSetRecoilState } from "recoil";
+import { authModalState } from "../../recoil/atoms/authModalAtom";
+import { Community } from "../../recoil/atoms/communitiesAtom";
+import { firestore } from "../../firebase/init";
+import PostLoader from "./PostLoader";
+import {Post, postState, PostVote} from "../../recoil/atoms/postAtom";
---
+++ components/post/HomePosts.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HomePosts: React.FC = () => {
+    const [loading, setLoading] = React.useState(false);
+
+    return (
+        <>
+            Home post component
+        </>
+    )
+}
+
+export default HomePosts;
