413a607d56adc100157494a92ec782b2397ff808
Peterdinis <peterdinis0@pobox.sk>
2022-12-29 22:20:25 +0100

    FOO

--- pages/r/[community]/submit.tsx
+++ pages/r/[community]/submit.tsx
@@ -1,21 +1,11 @@
 import { NextPage } from "next";
 import PageContent from "../../../components/shared/PageContent";
-import NewPostForm from "../../../components/posts/NewPostForm";
-import AboutWrapper from "../../../components/posts/AboutWrapper";
 import { Box, Text } from "@chakra-ui/react";
 
 const SubmitPage: NextPage = () => {
     return (
         <PageContent>
-          <>
-            <Box p="14px 0px" borderBottom="1px solid" borderColor="white">
-                <Text>Create a Post</Text>
-            </Box>
-            <NewPostForm />
-          </>
-          <>
-            <AboutWrapper />
-          </>
+         foof
         </PageContent>
     )
 }
