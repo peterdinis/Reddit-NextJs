1c7bb7ca2334c410865ab8e5f19be75552b7b57d
Peterdinis <peterdinis0@pobox.sk>
2022-12-29 20:36:27 +0100

    feat: Communities wip

--- components/communities/RecommendedCommunity.tsx
+++ components/communities/RecommendedCommunity.tsx
@@ -1,3 +1,19 @@
+import {
+    Box,
+    Button,
+    Flex,
+    Icon,
+    Image,
+    Skeleton,
+    SkeletonCircle,
+    Stack,
+    Text,
+  } from "@chakra-ui/react";
+  import { collection, getDocs, limit, orderBy, query } from "firebase/firestore";
+import React from "react";
+import Link from "next/link";
+import { FaReddit } from "react-icons/fa";
+
 const RecommendedCommunity: React.FC = () => {
     return (
         <>
