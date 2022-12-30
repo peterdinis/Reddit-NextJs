3a46115f6f914c53ef32d6459f0c7284bbc388b2
Peterdinis <peterdinis0@pobox.sk>
2022-12-30 14:50:24 +0100

    fix: Fixing one recoil problem wip

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,32 @@ import { theme } from "../chakra/chakraTheme";
 import Layout from "../components/shared/Layout";
 import { RecoilRoot } from "recoil";
 
+const memoize = (fn: any) => {
+  let cache = {} as any;
+  return (...args: any) => {
+    let n = args[0];
+    if (n in cache) {
+      return cache[n];
+    }
+    else {
+      let result = fn(n);
+      cache[n] = result;
+      return result;
+    }
+  }
+}
+
+
+// ignore in-browser next/js recoil warnings until its fixed.
+const mutedConsole = memoize((console: any) => ({
+  ...console,
+  warn: (...args: any) => args[0].includes('Duplicate atom key')
+    ? null
+    : console.warn(...args)
+}))
+
+global.console = mutedConsole(global.console);
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
