919e5ac3571245e3861e52deb6470069f1813948
Peterdinis <peterdinis0@pobox.sk>
2022-12-31 14:44:59 +0100

    feat: Updating layout

--- components/shared/Layout.tsx
+++ components/shared/Layout.tsx
@@ -1,22 +1,22 @@
-import React from "react"
+import React from "react";
 import Navbar from "../navbar/Navbar";
-import Meta from "./Meta";
+import Head from "next/head";
 
 interface IProps {
-    children?: React.ReactNode;
+  children?: React.ReactNode;
 }
 
 function Layout(props: IProps) {
-    return (
-        <>
-            <Meta 
-                title="Reddit - Dive into anything"
-                image="https://play-lh.googleusercontent.com/nlptFyxNsb8J0g8ZLux6016kunduV4jCxIrOJ7EEy-IobSN1RCDXAJ6DTGP81z7rr5Zq"
-            />
-            <Navbar />
-            {props.children}
-        </>
-    )
+  return (
+    <>
+      <Head>
+        <title>Reddit - Dive into anything</title>
+        <link rel="shortcut icon" href="/images/redditFace.svg" />
+      </Head>
+      <Navbar />
+      {props.children}
+    </>
+  );
 }
 
-export default Layout
+export default Layout;
