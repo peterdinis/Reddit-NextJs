0e875d247dfd1bb0f70c031c952719f170a9c292
Peterdinis <peterdinis0@pobox.sk>
2022-12-31 18:29:44 +0100

    feat: Start removing any in catch block wip

---
+++ utils/errorTyping.ts
@@ -0,0 +1,26 @@
+export type ErrorWithMessage = {
+  message: string;
+};
+
+function isErrorWithMessage(error: unknown): error is ErrorWithMessage {
+  return (
+    typeof error === "object" &&
+    error !== null &&
+    "message" in error &&
+    typeof (error as Record<string, unknown>).message === "string"
+  );
+}
+
+function toErrorWithMessage(maybeError: unknown): ErrorWithMessage {
+  if (isErrorWithMessage(maybeError)) return maybeError;
+
+  try {
+    return new Error(JSON.stringify(maybeError));
+  } catch {
+    return new Error(String(maybeError));
+  }
+}
+
+function getErrorMessage(error: unknown) {
+  return toErrorWithMessage(error).message;
+}
