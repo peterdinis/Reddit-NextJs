745ddd1baed44e85f18ed7461a0b03fac6e9d7b5
Peterdinis <peterdinis0@pobox.sk>
2022-12-31 14:46:03 +0100

    feat: Updating useScroll hook

--- hooks/useScroll.tsx
+++ hooks/useScroll.tsx
@@ -1,6 +1,7 @@
 
 import React from "react";
 import styles from "../styles/Component.module.css";
+import { Text } from "@chakra-ui/react";
 
 export default function ScrollToTop() {
   const [isVisible, setIsVisible] = React.useState<Boolean>(false);
@@ -33,7 +34,7 @@ export default function ScrollToTop() {
           onClick={scrollToTop}
           className={styles.iconWrapper}
         >
-          Back to top
+          <Text>Back to top</Text>
         </div>
       )}
     </div>
