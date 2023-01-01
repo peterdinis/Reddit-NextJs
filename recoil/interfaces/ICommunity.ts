c32227978a496818a6dd858b7f2657438b903cfe
Peterdinis <peterdinis0@pobox.sk>
2023-01-01 20:38:35 +0100

    feat: Start to updating interfaces wip

---
+++ recoil/interfaces/ICommunity.ts
@@ -0,0 +1,31 @@
+import { Timestamp } from "firebase/firestore";
+
+export interface Community {
+  id: string;
+  creatorId: string;
+  numberOfMembers: number;
+  privacyType: "public" | "restrictied" | "private";
+  createdAt?: Timestamp;
+  imageURL?: string;
+}
+
+export interface CommunitySnippet {
+  communityId: string;
+  isModerator?: boolean;
+  imageURL?: string;
+}
+
+export interface CommunityState {
+  [key: string]:
+    | CommunitySnippet[]
+    | { [key: string]: Community }
+    | Community
+    | boolean
+    | undefined;
+  mySnippets: CommunitySnippet[];
+  initSnippetsFetched: boolean;
+  visitedCommunities: {
+    [key: string]: Community;
+  };
+  currentCommunity: Community;
+}
